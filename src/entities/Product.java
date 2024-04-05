bdf6e0405aed4f42546cdaf053dd502b32b1f4a7
Vinicius Fillos <vinifillos@gmail.com>
2024-04-05 10:32:07 -0300

    Method reference static

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -26,6 +26,10 @@ public class Product {
         this.price = price;
     }
 
+    public static boolean staticPredicate(Product p) {
+        return p.getPrice() >= 100.0;
+    }
+
     @Override
     public String toString() {
         return name +", "+ price;
